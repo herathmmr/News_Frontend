47c2bf4bbb3d633aa272dc89a4fee9c48133dea3
Malshan Herath <mmrherath@students.nsbm.ac.lk>
2025-09-03 11:07:34 +0530

    implement the  BrowserRouter

--- news/src/App.jsx
+++ news/src/App.jsx
@@ -1,14 +1,19 @@
 import AdminPage from '../components/adminPage'
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
   
 
   return (
-    <>
-    <AdminPage />
-    </>
-  )
+    <BrowserRouter>
+      <Routes path="/">
+        <Route path="/admin" element={<AdminPage />} />
+        <Route path="/" element={<h1>home</h1>} />
+        <Route path= "*" element={<h1>not found</h1>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
